38dce1c21347b2ce1ec538c30eef50b0d87f566f
Jerome MR <10962717+Assasquid@users.noreply.github.com>
2021-04-03 19:01:14 +0100

    Splash Screen is displayed before loading Title Screen
    The Splash Screen is displayed for 4 seconds before the title screens loads instead.

---
+++ L_GlitchGarden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int loadingTime = 3;
+    int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(loadingTime);
+        LoadNextScene();
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
