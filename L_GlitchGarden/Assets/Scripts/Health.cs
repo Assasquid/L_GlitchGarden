ec5e313b884514affe251a8888b0a6717f122d1d
Jerome MR <10962717+Assasquid@users.noreply.github.com>
2021-05-10 23:47:35 +0100

    Defender Shoots and Destroys Enemies
    The Cactus Defender can now shoot a Projectile. Attackers now have Health and are destroyed when it reaches 0. On death they are destroyed and a particle effect plays.

---
+++ L_GlitchGarden/Assets/Scripts/Health.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+    [SerializeField] GameObject deathVFX;
+
+    public void DealDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            PlayDeathVFX();
+            Destroy(gameObject);
+        }
+
+        void PlayDeathVFX()
+        {
+            if (!deathVFX) { return; }
+
+            //Some parts have been commented out because the Particle System can handle destruction itself !
+            //GameObject = deathVFXObject = Instantiate(deathVFX, transform.position, Quaternion.identity);
+            Instantiate(deathVFX, transform.position, Quaternion.identity);
+            //Destroy(deathVFXObject, 1f);
+        }
+    }
+}
